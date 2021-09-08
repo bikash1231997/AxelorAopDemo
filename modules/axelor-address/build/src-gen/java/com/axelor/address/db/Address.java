fa9f230098b27587de2a819593a159e1b87dab1b
Bikash <bikashmohapatra1997@gmail.com>
2021-09-08 13:07:57 +0530

    sixth commit

--- modules/axelor-address/build/src-gen/java/com/axelor/address/db/Address.java
+++ modules/axelor-address/build/src-gen/java/com/axelor/address/db/Address.java
@@ -1,5 +1,6 @@
 package com.axelor.address.db;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 import javax.persistence.Basic;
@@ -9,8 +10,11 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -22,13 +26,16 @@ import com.google.common.base.MoreObjects;
 
 @Entity
 @Cacheable
-@Table(name = "ADDRESS_ADDRESS")
+@Table(name = "ADDRESS_ADDRESS", indexes = { @Index(columnList = "name") })
 public class Address extends AuditableModel {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ADDRESS_ADDRESS_SEQ")
 	@SequenceGenerator(name = "ADDRESS_ADDRESS_SEQ", sequenceName = "ADDRESS_ADDRESS_SEQ", allocationSize = 1)
 	private Long id;
+
+	@NotNull
+	private String name;
 
 	@NotNull
 	@Size(max = 255)
@@ -45,12 +52,27 @@ public class Address extends AuditableModel {
 
 	private String country;
 
+	@Widget(selection = "address.number.type")
+	private Integer kanbannumber = 0;
+
+	private LocalDate startDate;
+
+	private LocalDate endDate;
+
+	@Min(0)
+	@Max(4)
+	private Integer priority = 0;
+
 	@Widget(title = "Attributes")
 	@Basic(fetch = FetchType.LAZY)
 	@Type(type = "json")
 	private String attrs;
 
 	public Address() {
+	}
+
+	public Address(String name) {
+		this.name = name;
 	}
 
 	@Override
@@ -61,6 +83,14 @@ public class Address extends AuditableModel {
 	@Override
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getStreet() {
@@ -111,6 +141,38 @@ public class Address extends AuditableModel {
 		this.country = country;
 	}
 
+	public Integer getKanbannumber() {
+		return kanbannumber == null ? 0 : kanbannumber;
+	}
+
+	public void setKanbannumber(Integer kanbannumber) {
+		this.kanbannumber = kanbannumber;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public Integer getPriority() {
+		return priority == null ? 0 : priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
 	public String getAttrs() {
 		return attrs;
 	}
@@ -142,12 +204,17 @@ public class Address extends AuditableModel {
 	public String toString() {
 		return MoreObjects.toStringHelper(this)
 			.add("id", getId())
+			.add("name", getName())
 			.add("street", getStreet())
 			.add("area", getArea())
 			.add("city", getCity())
 			.add("zip", getZip())
 			.add("state", getState())
 			.add("country", getCountry())
+			.add("kanbannumber", getKanbannumber())
+			.add("startDate", getStartDate())
+			.add("endDate", getEndDate())
+			.add("priority", getPriority())
 			.omitNullValues()
 			.toString();
 	}
--- modules/axelor-address/build/src-gen/java/com/axelor/address/db/repo/AddressRepository.java
+++ modules/axelor-address/build/src-gen/java/com/axelor/address/db/repo/AddressRepository.java
@@ -2,11 +2,19 @@ package com.axelor.address.db.repo;
 
 import com.axelor.address.db.Address;
 import com.axelor.db.JpaRepository;
+import com.axelor.db.Query;
 
 public class AddressRepository extends JpaRepository<Address> {
 
 	public AddressRepository() {
 		super(Address.class);
+	}
+
+	public Address findByName(String name) {
+		return Query.of(Address.class)
+				.filter("self.name = :name")
+				.bind("name", name)
+				.fetchOne();
 	}
 
 }
